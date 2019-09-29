65ba22d6ffbccd5d77e400bf74a4a2f286e4f4b6
Sagar Pednekar <sagarpednekar04@gmail.com>
2019-09-29 20:00:56 +0530

    Fix(Typo in CORS middleware configurations)

--- config/application.rb
+++ config/application.rb
@@ -40,7 +40,7 @@ module EmailServiceBackend
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource('*', headers: any, methods: %i[get post put delete options])
+        resource('*', headers: :any, methods: %i[get post put delete options])
       end
     end
   end
